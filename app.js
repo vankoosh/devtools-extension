a853ea243f180663586c8119d08281b585cd7983
vankoosh <vankoosh@gmail.com>
2024-06-14 23:12:31 +0200

    horizontal lines work

--- app.js
+++ app.js
@@ -11,45 +11,50 @@ document.addEventListener("DOMContentLoaded", function (e) {
           function: () => {
             const horizontalLine = document.createElement("span");
             horizontalLine.style.width = '100vw';
-            horizontalLine.style.height = '2px';
+            horizontalLine.style.height = '1px';
             horizontalLine.style.backgroundColor = 'black';
             horizontalLine.style.zIndex = '9999';
-            horizontalLine.style.position = 'absolute';
+            horizontalLine.style.position = 'fixed';
             horizontalLine.style.top = '50%';
             horizontalLine.style.left = '0';
-            horizontalLine.style.cursor = 'pointer';
+            horizontalLine.style.cursor = 'move';
             horizontalLine.style.margin = '10px';
             horizontalLine.setAttribute("class", "horizontal-line")
-
             document.body.appendChild(horizontalLine);
+            let isClicked = false;
+            let mousePosition;
 
-            let isDragging = false;
-            let startY = 0;
-            let startTop = 0;
+            function moveLine(e) {
+              mousePosition = e.clientY;
+              horizontalLine.style.top = `${mousePosition - 10}px`;
+              document.body.style.cursor = "move";
+            }
 
-            // Event handler for mousedown event
-            horizontalLine.addEventListener('mousedown', (e) => {
-              isDragging = true;
-              startY = e.clientY;
-              startTop = horizontalLine.offsetTop;
-              document.body.style.cursor = 'pointer'; // Change cursor while dragging
-            });
+            horizontalLine.addEventListener("click", (e) => {
+              isClicked = !isClicked;
+              if (isClicked) {
+                document.addEventListener('mousemove', moveLine);
+              } else {
+                document.removeEventListener("mousemove", moveLine);
+                document.body.style.cursor = "default";
+              }
+            })
 
             // Event handler for mousemove event
-            document.addEventListener('mousemove', (e) => {
-              if (isDragging) {
-                const deltaY = e.clientY - startY;
-                horizontalLine.style.top = `${startTop + deltaY}px`;
-              }
-            });
+            // document.addEventListener('mousemove', (e) => {
+            //   if (isDragging) {
+            //     const deltaY = e.clientY - startY;
+            //     horizontalLine.style.top = `${startTop + deltaY}px`;
+            //   }
+            // });
 
             // Event handler for mouseup event
-            document.addEventListener('mouseup', () => {
-              if (isDragging) {
-                isDragging = false;
-                document.body.style.cursor = 'default'; // Revert cursor after dragging
-              }
-            });
+            // document.addEventListener('mouseup', () => {
+            //   if (isDragging) {
+            //     isDragging = false;
+            //     document.body.style.cursor = 'default'; // Revert cursor after dragging
+            //   }
+            // });
           },
         }
       );
@@ -64,7 +69,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
           target: { tabId: tabId },
           function: () => {
             const allLines = document.querySelectorAll(".horizontal-line");
-            console.log(allLines);
             allLines.forEach(element => {
               element.remove();
             });
