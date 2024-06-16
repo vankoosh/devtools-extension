71d13936cd68ee7fdad0e03165fe3b631ec7e1bd
vankoosh <vankoosh@gmail.com>
2024-06-16 15:51:12 +0200

    calculates width and height

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
   const addHORLineBtn = document.getElementById("addHLine");
   const addVERLineBtn = document.getElementById("addVLine");
   const deleteLinesBtn = document.getElementById("deleteLines");
+  const addRulerBtn = document.querySelector("#addRuler");
 
   addHORLineBtn.addEventListener("click", () => {
     chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
@@ -19,7 +20,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
             horizontalLine.style.top = '50%';
             horizontalLine.style.left = '0';
             horizontalLine.style.cursor = 'move';
-            // horizontalLine.style.margin = '10px';
             horizontalLine.setAttribute("class", "horizontal-line")
             document.body.appendChild(horizontalLine);
             let isClicked = false;
@@ -62,7 +62,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
             verticalLine.style.top = '0';
             verticalLine.style.left = '50%';
             verticalLine.style.cursor = 'move';
-            // verticalLine.style.margin = '10px';
             verticalLine.setAttribute("class", "vertical-line")
             document.body.appendChild(verticalLine);
             let isClicked = false;
@@ -106,5 +105,90 @@ document.addEventListener("DOMContentLoaded", function (e) {
         }
       );
     });
+  })
+
+  addRulerBtn.addEventListener("click", () => {
+    chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+      const tabId = tabs[0].id;
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tabId },
+          function: () => {
+            const verticalLineOne = document.createElement("span");
+            verticalLineOne.style.width = '1px';
+            verticalLineOne.style.height = '100vh';
+            verticalLineOne.style.backgroundColor = 'black';
+            verticalLineOne.style.zIndex = '9999';
+            verticalLineOne.style.position = 'fixed';
+            verticalLineOne.style.top = '0';
+            verticalLineOne.style.left = '40%';
+            verticalLineOne.style.cursor = 'move';
+            verticalLineOne.setAttribute("id", "vertical-line-one")
+            verticalLineOne.setAttribute("class", "ver-ruler-line")
+            document.body.appendChild(verticalLineOne);
+
+            const verticalLineTwo = document.createElement("span");
+            verticalLineTwo.style.width = '1px';
+            verticalLineTwo.style.height = '100vh';
+            verticalLineTwo.style.backgroundColor = 'black';
+            verticalLineTwo.style.zIndex = '9999';
+            verticalLineTwo.style.position = 'fixed';
+            verticalLineTwo.style.top = '0';
+            verticalLineTwo.style.left = '60%';
+            verticalLineTwo.style.cursor = 'move';
+            verticalLineTwo.setAttribute("id", "vertical-line-two")
+            verticalLineTwo.setAttribute("class", "ver-ruler-line")
+            document.body.appendChild(verticalLineTwo);
+
+            const horizontalLineOne = document.createElement("span");
+            horizontalLineOne.style.width = '100vw';
+            horizontalLineOne.style.height = '1px';
+            horizontalLineOne.style.backgroundColor = 'black';
+            horizontalLineOne.style.zIndex = '9999';
+            horizontalLineOne.style.position = 'fixed';
+            horizontalLineOne.style.top = '40%';
+            horizontalLineOne.style.left = '0';
+            horizontalLineOne.style.cursor = 'move';
+            horizontalLineOne.setAttribute("id", "horizontal-line-one")
+            horizontalLineOne.setAttribute("class", "hor-ruler-line")
+            document.body.appendChild(horizontalLineOne);
+
+            const horizontalLineTwo = document.createElement("span");
+            horizontalLineTwo.style.width = '100vw';
+            horizontalLineTwo.style.height = '1px';
+            horizontalLineTwo.style.backgroundColor = 'black';
+            horizontalLineTwo.style.zIndex = '9999';
+            horizontalLineTwo.style.position = 'fixed';
+            horizontalLineTwo.style.top = '60%';
+            horizontalLineTwo.style.left = '0';
+            horizontalLineTwo.style.cursor = 'move';
+            horizontalLineTwo.setAttribute("id", "horizontal-line-two")
+            horizontalLineTwo.setAttribute("class", "hor-ruler-line")
+            document.body.appendChild(horizontalLineTwo);
+
+            const showHeight = () => {
+              const lines = document.querySelectorAll(".hor-ruler-line")
+              let lineOneFromTop = lines[0].getBoundingClientRect().top.toFixed(0)
+              let lineTwoFromTop = lines[1].getBoundingClientRect().top.toFixed(0)
+              let height = Math.abs(lineOneFromTop - lineTwoFromTop)
+              console.log(height);
+            }
+
+            const showWidth = () => {
+              const lines = document.querySelectorAll(".ver-ruler-line")
+              let lineOneFromLeft = lines[0].getBoundingClientRect().left.toFixed(0)
+              let lineTwoFromLeft = lines[1].getBoundingClientRect().left.toFixed(0)
+              let width = Math.abs(lineOneFromLeft - lineTwoFromLeft)
+              console.log(width);
+            }
+
+            showHeight();
+            showWidth();
+
+          },
+        }
+      );
+    });
+
   })
 });
