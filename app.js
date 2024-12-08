bdc6ab389bc3be516d28257ca62823d2077c55c7
vankoosh <vankoosh@gmail.com>
2024-12-08 23:19:32 +0100

    simple func now

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 const addRulerBtn = document.querySelector(".add-ruler-button");
 
-function addRulerCallback() {
-  chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
-    const tabId = tabs[0].id;
-    chrome.scripting.executeScript(
-      {
-        target: { tabId: tabId },
-        function: () => {
-          const body = document.querySelector("body");
-          console.log(body);
-        },
-      }
-    );
-  });
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   addRulerBtn.addEventListener("click", e => {
-    addRulerCallback()
+    chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+      const tabId = tabs[0].id;
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tabId },
+          function: () => {
+            const body = document.querySelector("body");
+            console.log(body);
+          },
+        }
+      );
+    });
   });
 });
